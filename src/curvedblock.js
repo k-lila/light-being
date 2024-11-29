295f04fd29cd3732f546f410ebeabd5718e6b760
k-lila <krishnalilapalandi@gmail.com>
2024-11-29 19:35:12 -0300

    random v

--- src/curvedblock.js
+++ src/curvedblock.js
@@ -9,13 +9,16 @@ export function curvedBlock(radius, size, angleA, angleB) {
     const curved_shape = new THREE.Shape();
     curved_shape.absarc(0, 0, radius, radB, radA, true);
     curved_shape.absarc(0, 0, radius + size, radA, radB, false);
+
     const extrudeSettings = {
         steps: 1,
         depth: 1,
         curveSegments: 4
     };
+
+
     const geometry = new THREE.ExtrudeGeometry( curved_shape, extrudeSettings );
-    const material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: true } );
+    const material = new THREE.MeshBasicMaterial( { color: 0xffffff, wireframe: false } );
     const mesh = new THREE.Mesh( geometry, material );
     return mesh
 }
