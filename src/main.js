a15beaf485a751e080de334af53c03dfc20b3933
k-lila <krishnalilapalandi@gmail.com>
2024-11-29 03:10:36 -0300

    ring generator

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import curvedBlock from './curvedblock.js';
+import ringGenerator from './ringgenerator.js';
+
 
 function main() {
 
@@ -12,47 +14,50 @@ function main() {
     const fov = 45;
     const aspect = window.innerWidth / window.innerHeight;
     const near = 0.1;
-    const far = 100;
+    const far = 1000;
     const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set( 0, 0, 40 );
+    camera.position.set( 0, 0, 75 );
     camera.lookAt( 0, 0, 0 );
 
 
     
     const scene = new THREE.Scene();
 
-    const teste = curvedBlock(7, 0, Math.PI * 2)
-    const teste2 = curvedBlock(10, 0, Math.PI / 2)
-    const teste3 = curvedBlock(13, 0, Math.PI / 2)
+    const mathPi = (Math.PI * 2) / 360
 
-    scene.add(teste);
 
-    scene.add(teste2);
+    const ring = ringGenerator(20, 2, 5)
+    const ring2 = ringGenerator(24, 2, 3)
+    const ring3 = ringGenerator(28, 2, 3)
 
-    scene.add(teste3);
-    
+    ring.forEach((e) => {
+        scene.add(e)
+    })
 
-    teste.rotation.x = 0;
-    teste.rotation.y = 0;
+
+    ring2.forEach((e) => {
+        scene.add(e)
+    })
+    ring3.forEach((e) => {
+        scene.add(e)
+    })
+
 
     renderer.render(scene, camera);
 
+
+
     function animate() {
-        teste.rotation.x += 0.01;
-        teste.rotation.y += 0.01;
-        teste.rotation.z += 0.01;
-        teste2.rotation.x += 0.0025;
-        teste2.rotation.y += 0.0025;
-        teste2.rotation.z += 0.0025;
-        teste3.rotation.x += 0.005;
-        teste3.rotation.y += 0.005;
-        teste3.rotation.z += 0.001;
-
-
+        ring.forEach((e, i) => {
+            e.rotation.z += 0.001
+        })
+        ring3.forEach((e, i) => {
+            e.rotation.z -= 0.001
+        })
         renderer.render( scene, camera );
         requestAnimationFrame(animate);
     }
-    animate()
+    // animate()
 
 }
 
