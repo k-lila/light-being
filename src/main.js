36e3f0568b0ab80fe0a6a85b8c98d693adecec90
k-lila <krishnalilapalandi@gmail.com>
2024-11-30 04:24:19 -0300

    OrthographicCamera

--- src/main.js
+++ src/main.js
@@ -9,25 +9,36 @@ function main() {
     const renderer = new THREE.WebGLRenderer({antialias: true, canvas});
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    const fov = 100;
+    // const fov = 100;
+    // const aspect = window.innerWidth / window.innerHeight;
+    // const near = 0.1;
+    // const far = 5000;
+    // const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    // camera.position.set( 0, 0, 500 );
+    // camera.lookAt( 0, 0, 500 );
+
     const aspect = window.innerWidth / window.innerHeight;
+    const frustumSize = 500;
+    
+    const left = -frustumSize * aspect / 2;
+    const right = frustumSize * aspect / 2;
+    const top = frustumSize / 2;
+    const bottom = -frustumSize / 2;
     const near = 0.1;
     const far = 5000;
-    const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    camera.position.set( 0, 0, 500 );
-    camera.lookAt( 0, 0, 500 );
+    
+    const camera = new THREE.OrthographicCamera(left, right, top, bottom, near, far);
+    camera.position.set(50, 50, 1000);
+    camera.lookAt(50, -150, 0);
 
     const scene = new THREE.Scene();
-
     const space = ringSpace();
-
 
     space.forEach((ring) => {
         ring.forEach((piece) => {
             scene.add(piece)
         })
     })
-
 
     renderer.render(scene, camera);
 
--- src/ringspace.js
+++ src/ringspace.js
@@ -1,15 +1,15 @@
 import ringGenerator from "./ringgenerator.js";
 
 function ringSpace() {
-    const sunDiameter = 50
-    const numRings = 200
+    const sunDiameter = 30
+    const numRings = 150
     let _num = 0
     const diameter_list = []
     for (let i = 0; i < numRings; i++) {
-        diameter_list.push(sunDiameter + (i * 10))
+        diameter_list.push(sunDiameter + (i * 3))
     }
     const ringspace = diameter_list.map((m, i) => {
-        return ringGenerator(m, 6.5, i + 10)
+        return ringGenerator(m, 2, i + 10)
     })
 
     return ringspace
