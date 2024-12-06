e890d47dfe778ca8a1d2c64c71c4d7e0986c5c58
k-lila <krishnalilapalandi@gmail.com>
2024-12-06 14:13:44 -0300

    wip

--- src/custommaterial.js
+++ src/custommaterial.js
@@ -5,7 +5,7 @@ function customMaterial() {
     const opacity = rand < 0.2 ? 0.2 : rand
     const material = new THREE.MeshLambertMaterial({
         color: 'rgba(0, 0, 0)',
-        emissive: `rgba(${Math.floor(rand * 255)}, ${Math.floor(rand * 255)}, ${Math.floor(rand * 255)})`,
+        emissive: `rgba(${Math.floor(rand * 155) + 100}, ${Math.floor(rand * 155) + 100}, ${Math.floor(rand * 155) + 100})`,
         emissiveIntensity: 0.3,
         transparent: true,
         opacity: opacity
